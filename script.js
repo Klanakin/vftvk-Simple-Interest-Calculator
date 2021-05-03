b04826607ef47186f0b752ebccf8ac5e9c054314
Don Klanakin <don.klanakin@gmail.com>
2021-05-03 14:52:53 +0000

    Modified script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ function compute(){
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
     var amount = parseInt(principal) + interest;
+
+    if (principal <= 0) {
+        alert("Enter a positive number")
+    }
+    
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 
